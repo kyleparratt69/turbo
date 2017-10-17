9593ebb896e56a6682e0bb441a1f15ac52a24d27
kyleparratt69 <parrotialism@gmail.com>
2017-10-17 13:15:08 +0200

    Added content grid on home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { JumbotronComponent } from './views/home/jumbotron/jumbotron.component';
 import { SideMenuComponent } from './views/home/side-menu/side-menu.component';
 import { SearchBarComponent } from './views/home/search-bar/search-bar.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ContentGridComponent } from './views/home/content-grid/content-grid.component';
+import { ContentPanelComponent } from './views/home/content-grid/content-panel/content-panel.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +20,9 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
     JumbotronComponent,
     SideMenuComponent,
     SearchBarComponent,
+    ContentGridComponent,
+    ContentPanelComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
