17ccd556998bc4e3bca6f17eea059c7ed6d625a8
kyleparratt69 <parrotialism@gmail.com>
2017-10-17 10:44:35 +0200

    Added and styled Jumbotron to home page and styled header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { JumbotronComponent } from './views/home/jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule
